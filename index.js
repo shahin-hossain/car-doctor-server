19c746d3ac06f32b201eab6fe53fa079c9320826
shahin hossain <shahinhossain3@hotmail.com>
2023-12-30 18:45:03 +0600

    Insert Bulk Data In Database And Load Services Data

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // send data with GET API
+        app.get('/services', async (req, res) => {
+            const carCollection = client.db('carDoctor').collection('services');
+            const cursor = carCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
